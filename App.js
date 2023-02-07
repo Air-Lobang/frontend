4a81f7c88c43e8f6f4efb5ca72b0f88a112947c2
Jun Hui <junhui.tan.2021@scis.smu.edu.sg>
2023-02-07 15:09:48 +0800

    made basic interface for signup page

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import CheckBox from '@react-native-community/checkbox';
+import React, {Component} from 'react';
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -6,21 +8,79 @@ import {
   SafeAreaView,
   Image,
   TextInput,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
 
+const initialState = {
+  react: false,
+  next: false,
+  vue: false,
+  angular: false,
+};
+
 export default function App() {
+  const [state, setState] = React.useState(initialState);
   return (
     <SafeAreaView style={styles.container}>
+      <Image source={require("./assets/signuptop_icon.png")} 
+      />
       <View style={styles.form}>
+
         <TextInput 
           style={styles.input} 
           placeholder="Email">
         </TextInput>
+
+        <View style={{flexDirection: 'row'}}>
           <TextInput 
             style={styles.input2} 
             placeholder="First Name">
           </TextInput>
+          <TextInput 
+            style={styles.input2} 
+            placeholder="Last Name">
+          </TextInput>
+        </View>
+
+        <TextInput 
+          style={styles.input} 
+          placeholder="Phone Number">
+        </TextInput>
+
+        <TextInput 
+          style={styles.input} 
+          placeholder="Password">
+        </TextInput>
+
+        <TextInput 
+          style={styles.input} 
+          placeholder="Confirm Password">
+        </TextInput>
+
       </View>
+
+      <View style={{flexDirection: 'row', }}>
+        <Text style={{textDecorationLine:'underline'}}>
+          I agree to the terms and conditions
+        </Text>
+      </View>
+
+        <TouchableOpacity onPress={() => Alert.alert("Submission", "Confirm submission?", [{text: "Yes"/*, onPress:...*/}, {text: "No"/*, onPress:...*/},])}>
+          <Image style={{
+            margin: 10,
+          }}
+          source={require("./assets/submit_button.png")}/>
+        </TouchableOpacity>
+
+        <Text 
+          style={{
+            textDecorationLine: 'underline'
+          }}
+          /*onPress=...*/>
+          Already a member? Log in
+        </Text>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -43,7 +103,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   input2: {
-    width: 140,
+    width: 130,
     height: 40,
     borderColor: "gray",
     borderWidth: 1,
@@ -53,5 +113,6 @@ const styles = StyleSheet.create({
   form: {
     backgroundColor: "#fff",
     justifyContent: 'space-between',
+    margin: 20,
   },
 });
