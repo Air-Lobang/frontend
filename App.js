24bde802a34d1f513b3b7153d528d494e60c1c18
Jun Hui <junhui.tan.2021@scis.smu.edu.sg>
2023-02-07 11:17:00 +0800

    sample form creation

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Email">
+        </TextInput>
+          <TextInput 
+            style={styles.input2} 
+            placeholder="First Name">
+          </TextInput>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: 280,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    alignItems: "center",
+    padding: 10,
+    margin: 10,
+  },
+  input2: {
+    width: 140,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+  },
+  form: {
+    backgroundColor: "#fff",
+    justifyContent: 'space-between',
   },
 });
